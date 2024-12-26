jpsdr/mxe-Windows-7

build.sh: script for building MKVToolNix-required packages

build.sh
@@ -1,0 +1,56 @@
+#!/bin/zsh
+
+set -e
+
+cd ${0:a:h}
+
+build_x86_64=1
+build_i686=1
+
+while [[ -n $1 ]]; do
+  case $1 in
+    --amd64|--x86_64|--x86-64|--64) build_i686=0   ;;
+    --i686|--32)                    build_x86_64=0 ;;
+    *)
+      echo "error: unknown option $1"
+      exit 1
+      ;;
+  esac
+
+  shift
+done
+
+dir=usr-$(git branch --show-current)
+mkdir -p ${dir}
+rm -f usr
+ln -s ${dir} usr
+
+cat > settings.mk <<EOF
+# DO NOT EDIT THIS FILE!
+# Edit build.sh instead.
+
+MXE_USE_CCACHE =
+MXE_PLUGIN_DIRS += plugins/gcc12
+
+MXE_TARGETS=
+#MXE_TARGETS+=x86_64-w64-mingw32.static
+#MXE_TARGETS+=i686-w64-mingw32.static
+#MXE_TARGETS+=x86_64-w64-mingw32.shared
+#MXE_TARGETS+=i686-w64-mingw32.shared
+
+JOBS=$(($(nproc)+1))
+#SOURCEFORGE_MIRROR = switch.dl.sourceforge.net
+MKVTOOLNIX_DEPENDENCIES=gettext libiconv zlib boost flac ogg pthreads vorbis cmark libdvdread gmp
+MKVTOOLNIX_DEPENDENCIES+=qt6 qt6-qtmultimedia
+
+LOCAL_PKG_LIST=\$(MKVTOOLNIX_DEPENDENCIES)
+local-pkg-list: \$(LOCAL_PKG_LIST)
+mkvtoolnix-deps: local-pkg-list
+EOF
+
+touch -d '1 year ago' settings.mk
+
+if (( $build_x86_64 )) nice -2 make MXE_TARGETS=x86_64-w64-mingw32.static mkvtoolnix-deps
+if (( $build_i686 ))   nice -2 make MXE_TARGETS=i686-w64-mingw32.static mkvtoolnix-deps
+
+echo Done.

jpsdr/mxe_QT6_Windows7

build.sh
@@ -1,0 +1,56 @@
+#!/bin/zsh
+
+set -e
+
+cd ${0:a:h}
+
+build_x86_64=1
+build_i686=1
+
+while [[ -n $1 ]]; do
+  case $1 in
+    --amd64|--x86_64|--x86-64|--64) build_i686=0   ;;
+    --i686|--32)                    build_x86_64=0 ;;
+    *)
+      echo "error: unknown option $1"
+      exit 1
+      ;;
+  esac
+
+  shift
+done
+
+dir=usr-$(git branch --show-current)
+mkdir -p ${dir}
+rm -f usr
+ln -s ${dir} usr
+
+cat > settings.mk <<EOF
+# DO NOT EDIT THIS FILE!
+# Edit build.sh instead.
+
+MXE_USE_CCACHE =
+MXE_PLUGIN_DIRS += plugins/gcc13
+
+MXE_TARGETS=
+#MXE_TARGETS+=x86_64-w64-mingw32.static
+#MXE_TARGETS+=i686-w64-mingw32.static
+#MXE_TARGETS+=x86_64-w64-mingw32.shared
+#MXE_TARGETS+=i686-w64-mingw32.shared
+
+JOBS=$(($(nproc)+1))
+#SOURCEFORGE_MIRROR = switch.dl.sourceforge.net
+MKVTOOLNIX_DEPENDENCIES=gettext libiconv zlib boost flac ogg pthreads vorbis cmark libdvdread gmp
+MKVTOOLNIX_DEPENDENCIES+=qt6 qt6-qtmultimedia
+
+LOCAL_PKG_LIST=\$(MKVTOOLNIX_DEPENDENCIES)
+local-pkg-list: \$(LOCAL_PKG_LIST)
+mkvtoolnix-deps: local-pkg-list
+EOF
+
+touch -d '1 year ago' settings.mk
+
+if (( $build_x86_64 )) nice -2 make MXE_TARGETS=x86_64-w64-mingw32.static mkvtoolnix-deps
+if (( $build_i686 ))   nice -2 make MXE_TARGETS=i686-w64-mingw32.static mkvtoolnix-deps
+
+echo Done.